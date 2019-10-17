6b7258db3acc899c0f098b146d75516508443e38
Bernardo Vieira <bernardo5@live.com.pt>
2019-10-17 22:38:26 +0100

    downgrade typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Main from './Pages/Main/Main';
 
 
 class App extends Component {
     public render() {
         return (
-            <Router>
-                <Route path="/" exact={true} component={Main} />
-            </Router>
+            <Main />
         );
     }
 }
