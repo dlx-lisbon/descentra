586da23b13b6f878c1a122d0dfcd109cbba99a49
Bernardo Vieira <bernardo5@live.com.pt>
2019-10-19 20:29:26 +0100

    add countdown template

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Countdown, { CountdownTimeDelta } from 'react-countdown-now';
 
 
 /**
@@ -10,158 +11,67 @@ class Main extends Component<{}, {}> {
      */
     public render() {
         return (
-            <div>
-                {/*<!-- Navigation -->*/}
-                <a className="menu-toggle rounded" href="#">
-                    <i className="fas fa-bars" />
-                </a>
-                <nav id="sidebar-wrapper">
-                    <ul className="sidebar-nav">
-                        <li className="sidebar-brand">
-                            <a className="js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-                        </li>
-                        <li className="sidebar-nav-item">
-                            <a className="js-scroll-trigger" href="#page-top">Home</a>
-                        </li>
-                        <li className="sidebar-nav-item">
-                            <a className="js-scroll-trigger" href="#about">About</a>
-                        </li>
-                        <li className="sidebar-nav-item">
-                            <a className="js-scroll-trigger" href="#services">Services</a>
-                        </li>
-                        <li className="sidebar-nav-item">
-                            <a className="js-scroll-trigger" href="#portfolio">Portfolio</a>
-                        </li>
-                        <li className="sidebar-nav-item">
-                            <a className="js-scroll-trigger" href="#contact">Contact</a>
-                        </li>
-                    </ul>
-                </nav>
+            <div className="bg-g1 size1 flex-w flex-col-c-sb p-l-15 p-r-15 p-t-55 p-b-35 respon1">
+                <span />
+                <div className="flex-col-c p-t-50 p-b-50">
+                    <h3 className="l1-txt1 txt-center p-b-10">
+                        Coming Soon
+                    </h3>
 
-                {/* <!-- Header --> */}
-                <header className="masthead d-flex">
-                    <div className="container text-center my-auto">
-                        <h1 className="mb-1">Stylish Portfolio</h1>
-                        <h3 className="mb-5">
-                            <em>A Free Bootstrap Theme by Start Bootstrap</em>
-                        </h3>
-                        <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
-                    </div>
-                    <div className="overlay" />
-                </header>
+                    <p className="txt-center l1-txt2 p-b-60">
+                        Our website is under construction
+                    </p>
 
-                {/* <!-- About --> */}
-                <section className="content-section bg-light" id="about">
-                    <div className="container text-center">
-                        <div className="row">
-                            <div className="col-lg-10 mx-auto">
-                                <h2>Stylish Portfolio is the perfect theme for your next project!</h2>
-                                <p className="lead mb-5">
-                                    This theme features a flexible,
-                                    UX friendly sidebar menu and stock photos from
-                                    our friends at
-                                    <a href="https://unsplash.com/">Unsplash</a>!</p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                    <Countdown
+                        date={new Date(2019, 12, 5, 19, 0, 0)}
+                        renderer={this.renderCountdown}
+                    />
 
-                {/* <!-- Portfolio --> */}
-                <section className="content-section" id="portfolio">
-                    <div className="container">
-                        <div className="content-section-heading text-center">
-                            <h3 className="text-secondary mb-0">Portfolio</h3>
-                            <h2 className="mb-5">Recent Projects</h2>
-                        </div>
-                        <div className="row no-gutters">
-                            <div className="col-lg-6">
-                                <a className="portfolio-item" href="#">
-                                    <span className="caption">
-                                        <span className="caption-content">
-                                            <h2>Stationary</h2>
-                                            <p className="mb-0">
-                                                A yellow pencil with envelopes on a clean, blue backdrop!
-                                            </p>
-                                        </span>
-                                    </span>
-                                    <img className="img-fluid" src="img/portfolio-1.jpg" alt="" />
-                                </a>
-                            </div>
-                            <div className="col-lg-6">
-                                <a className="portfolio-item" href="#">
-                                    <span className="caption">
-                                        <span className="caption-content">
-                                            <h2>Ice Cream</h2>
-                                            <p className="mb-0">
-                                                A dark blue background with a colored pencil, a clip, and a tiny ice
-                                                cream cone!
-                                            </p>
-                                        </span>
-                                    </span>
-                                    <img className="img-fluid" src="img/portfolio-2.jpg" alt="" />
-                                </a>
-                            </div>
-                            <div className="col-lg-6">
-                                <a className="portfolio-item" href="#">
-                                    <span className="caption">
-                                        <span className="caption-content">
-                                            <h2>Strawberries</h2>
-                                            <p className="mb-0">
-                                                Strawberries are such a tasty snack, especially with a little sugar on
-                                                top!
-                                            </p>
-                                        </span>
-                                    </span>
-                                    <img className="img-fluid" src="img/portfolio-3.jpg" alt="" />
-                                </a>
-                            </div>
-                            <div className="col-lg-6">
-                                <a className="portfolio-item" href="#">
-                                    <span className="caption">
-                                        <span className="caption-content">
-                                            <h2>Workspace</h2>
-                                            <p className="mb-0">
-                                                A yellow workspace with some scissors, pencils, and other objects.
-                                            </p>
-                                        </span>
-                                    </span>
-                                    <img className="img-fluid" src="img/portfolio-4.jpg" alt="" />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                    <button className="flex-c-m s1-txt2 size3 how-btn" data-toggle="modal" data-target="#subscribe">
+                        Follow us for update now!
+                    </button>
+                </div>
 
-                {/* <!-- Footer --> */}
-                <footer className="footer text-center">
-                    <div className="container">
-                        <ul className="list-inline mb-5">
-                            <li className="list-inline-item">
-                                <a className="social-link rounded-circle text-white mr-3" href="#">
-                                    <i className="icon-social-facebook" />
-                                </a>
-                            </li>
-                            <li className="list-inline-item">
-                                <a className="social-link rounded-circle text-white mr-3" href="#">
-                                    <i className="icon-social-twitter" />
-                                </a>
-                            </li>
-                            <li className="list-inline-item">
-                                <a className="social-link rounded-circle text-white" href="#">
-                                    <i className="icon-social-github" />
-                                </a>
-                            </li>
-                        </ul>
-                        <p className="text-muted small mb-0">Copyright &copy; Your Website 2019</p>
-                    </div>
-                </footer>
-
-                {/* <!-- Scroll to Top Button--> */}
-                <a className="scroll-to-top rounded js-scroll-trigger" href="#page-top">
-                    <i className="fas fa-angle-up" />
-                </a>
+                <span className="s1-txt3 txt-center">
+                    @ 2017 Coming Soon Template. Designed by Colorlib
+                </span>
             </div>
         );
+    }
+
+    /**
+     * render the countdown
+     */
+    private renderCountdown = (countdown: CountdownTimeDelta) => {
+        if (countdown.completed) {
+            // Render a completed state
+            return 'Done!';
+        } else {
+            // Render a countdown
+            return (
+                <div className="flex-w flex-c cd100 p-b-82">
+                    <div className="flex-col-c-m size2 how-countdown">
+                        <span className="l1-txt3 p-b-9 days">{countdown.days}</span>
+                        <span className="s1-txt1">Days</span>
+                    </div>
+
+                    <div className="flex-col-c-m size2 how-countdown">
+                        <span className="l1-txt3 p-b-9 hours">{countdown.hours}</span>
+                        <span className="s1-txt1">Hours</span>
+                    </div>
+
+                    <div className="flex-col-c-m size2 how-countdown">
+                        <span className="l1-txt3 p-b-9 minutes">{countdown.minutes}</span>
+                        <span className="s1-txt1">Minutes</span>
+                    </div>
+
+                    <div className="flex-col-c-m size2 how-countdown">
+                        <span className="l1-txt3 p-b-9 seconds">{countdown.seconds}</span>
+                        <span className="s1-txt1">Seconds</span>
+                    </div>
+                </div>
+            );
+        }
     }
 }
 
