8679f4e9fbcf660a3a47705675becbd664073980
Bernardo Vieira <bernardo5@live.com.pt>
2019-12-16 09:39:53 +0000

    add kudos page template

---
+++ src/routes/Kudos/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Col, Grid, Row } from 'rsuite';
+
+export default function Kudos() {
+    return (
+        <Grid fluid={true} style={{ padding: '50px' }}>
+            <Row className="show-grid">
+                <Col xs={4}><img src="https://via.placeholder.com/100" alt="example" /></Col>
+                <Col xs={4}><img src="https://via.placeholder.com/100" alt="example" /></Col>
+                <Col xs={4}><img src="https://via.placeholder.com/100" alt="example" /></Col>
+                <Col xs={4}><img src="https://via.placeholder.com/100" alt="example" /></Col>
+                <Col xs={4}><img src="https://via.placeholder.com/100" alt="example" /></Col>
+                <Col xs={4}><img src="https://via.placeholder.com/100" alt="example" /></Col>
+            </Row>
+            <br />
+            <Row className="show-grid">
+                <Col xs={4}><img src="https://via.placeholder.com/100" alt="example" /></Col>
+                <Col xs={4}><img src="https://via.placeholder.com/100" alt="example" /></Col>
+                <Col xs={4}><img src="https://via.placeholder.com/100" alt="example" /></Col>
+                <Col xs={4}><img src="https://via.placeholder.com/100" alt="example" /></Col>
+                <Col xs={4}><img src="https://via.placeholder.com/100" alt="example" /></Col>
+                <Col xs={4}><img src="https://via.placeholder.com/100" alt="example" /></Col>
+            </Row>
+        </Grid>
+    );
+}
