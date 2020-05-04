8e108af953a35cad5fe87a0018df616f4bccc527
Bernardo Vieira <bernardo5@live.com.pt>
2020-05-04 17:26:36 +0100

    new navbar with drawer menu for mobile

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,23 @@
-import { makeStyles } from "@material-ui/core/styles";
 import { Menu, MenuItem, AppBar, Toolbar, Typography, Avatar, IconButton, Button } from "@material-ui/core";
 import MoreIcon from '@material-ui/icons/MoreVert';
 import React from "react";
 
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+
+const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
     root: {
         position: "absolute",
@@ -33,6 +48,34 @@ const useStyles = makeStyles((theme) => ({
             display: 'none',
         },
     },
+    appBar: {
+        display: 'contents',
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    burgerOpenMenuButton: {
+        marginRight: theme.spacing(2),
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
 }));
 
 export interface NavbarItem {
@@ -48,14 +91,15 @@ export interface NavbarProps {
 
 export default function Navbar(props: NavbarProps) {
     const classes = useStyles();
-    const [collapseEl, setCollapseEl] = React.useState(null);
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
 
-    const handleClick = (event: any) => {
-        setCollapseEl(event.currentTarget);
+    const handleDrawerOpen = () => {
+        setOpen(true);
     };
 
-    const handleClose = () => {
-        setCollapseEl(null);
+    const handleDrawerClose = () => {
+        setOpen(false);
     };
 
     const closeAll = (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
@@ -64,56 +108,67 @@ export default function Navbar(props: NavbarProps) {
     };
 
     const userAvatarSrc = 'img/unknown_user.svg';
-
     return (
-        <AppBar position="static" color="default" elevation={0}>
+        <AppBar
+            position="fixed"
+            className={classes.appBar}
+            color="default"
+            elevation={0}
+        >
             <Toolbar>
-                <img
-                    className={classes.logo}
-                    src="img/clown-fish.svg"
-                    alt="some clown fish"
-                    onClick={closeAll}
-                />
-                <Typography variant="h6" color="inherit" noWrap >
-                    Bem-vindo ao DLX
-                </Typography>
-                <div className={classes.grow} />
                 <div className={classes.sectionDesktop}>
+                    <img
+                        className={classes.logo}
+                        src="img/clown-fish.svg"
+                        alt="some clown fish"
+                        onClick={closeAll}
+                    />
                     {props.items.map((navItem: NavbarItem) => (
-                        <Button key={navItem.key} onClick={navItem.onClick}>
+                        <Button
+                            key={navItem.key}
+                            style={{ textTransform: 'none' }}
+                            onClick={navItem.onClick}
+                        >
                             {navItem.children}
                         </Button>
                     ))}
                 </div>
                 <div className={classes.sectionMobile}>
                     <IconButton
-                        aria-label="show more"
-                        aria-controls="simple-menu"
-                        aria-haspopup="true"
-                        onClick={handleClick}
                         color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={classes.burgerOpenMenuButton}
                     >
-                        <MoreIcon />
+                        <MenuIcon />
                     </IconButton>
                 </div>
-                <Menu
-                    id="simple-menu"
-                    anchorEl={collapseEl}
-                    keepMounted
-                    open={Boolean(collapseEl)}
-                    onClose={handleClose}
-                >
-                    {props.items.map((navItem: NavbarItem) => (
-                        <MenuItem key={navItem.key} onClick={() => {
-                            handleClose()
-                            navItem.onClick()
-                        }}>
-                            {navItem.children}
-                        </MenuItem>
-                    ))}
-                </Menu>
+                <div className={classes.grow} />
                 <Avatar onClick={props.onAvatarClick} src={userAvatarSrc} />
             </Toolbar>
+            <Drawer
+                className={classes.drawer}
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>
+                    {props.items.map((navItem: NavbarItem) => (
+                        <ListItem button key={navItem.key}>
+                            <ListItemText primary={navItem.children} onClick={navItem.onClick} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
         </AppBar>
     );
 }
