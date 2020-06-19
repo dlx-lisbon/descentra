67635c29730efb49b03b385adff4d9d5f036fe25
Conrado Fonseca <conradob@live.com>
2020-06-19 17:59:10 +0100

    Implementado wallet connect

--- src/components/drawers/Profile.tsx
+++ src/components/drawers/Profile.tsx
@@ -1,27 +1,12 @@
 import { Button, Typography } from '@material-ui/core';
-import { ethers } from 'ethers';
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../contexts/Auth';
 
 export default function Profile() {
-    const [userAddress, setUserAddress] = useState('');
-
-    useEffect(() => {
-        const getUserAddress = async () => {
-            if ((window as any).ethereum !== undefined) {
-                try {
-                    const provider = new ethers.providers.Web3Provider((window as any).ethereum);
-                    const signer = provider.getSigner();
-                    setUserAddress(await signer.getAddress());
-                } catch (error) {
-                    // not loggedin yet
-                }
-            }
-        };
-        getUserAddress();
-    }, []);
+    const { address, handleLogin, handleLogout } = useContext(AuthContext);
 
     const getUser = () => {
-        if (userAddress.length === 0) {
+        if (!address) {
             return (
                 <Typography variant="body2" gutterBottom>
                     Your are not loggedin!
@@ -30,20 +15,14 @@ export default function Profile() {
         }
         return (
             <Typography variant="body2" gutterBottom>
-                You address is {userAddress}
+                You address is {address}
             </Typography>
         );
     };
     return (
         <>
-            <Button
-                onClick={() => {
-                    console.log((window as any).ethereum, window as any);
-                    (window as any).ethereum.enable();
-                }}
-                color="primary"
-            >
-                Login
+            <Button onClick={!address ? handleLogin : handleLogout} color="primary">
+                {!address ? 'Login' : 'Logout'}
             </Button>
             {getUser()}
         </>
